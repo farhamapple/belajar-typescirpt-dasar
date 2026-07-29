a662bc1cda66db5197be1d5e5e743348432f1e2c
PC Farham <pc.farham@hotmail.com>
2026-07-29 20:16:48 +0700

    fix: console.info -> expect di seller test

--- tests/seller.test.ts
+++ tests/seller.test.ts
@@ -1,13 +1,28 @@
 import type { Seller } from "../src/seller"
-
 describe('Interface Seller', function(){
     it('should support in typescript', function(){
 
         const seller: Seller = {
             id: 1,
-            name: 'Toko ABC'
+            name: 'Toko ABC',
+            nib: '1234567890',
+            npwp: '1234567890'
         }
 
+        seller.name = "Toko XYZ";
+
         console.info(seller)
+    })
+
+    it('should support function interface', function(){
+        
+        interface AddFunction{
+            (value1: number, value2: number): number
+        }
+
+        const add : AddFunction = (value1: number, value2: number): number => {
+            return value1 + value2;
+        }
+        expect(add(1, 2)).toBe(3);
     })
 })

d509862c4da76e6d354d008367b3751632a1c7b1
PC Farham <pc.farham@hotmail.com>
2026-07-29 16:16:42 +0700

    chore: update config dan tambah test

---
+++ tests/array.test.ts
@@ -0,0 +1,11 @@
+describe('Array', function(){
+    it('should return array', function(){
+        const names: string[] = ["Eko", "Kurniawan", "Khannedy"];
+        //expect(names).toEqual(["Eko", "Kurniawan", "Khannedy"]);
+
+        const value: number[] = [1, 2, 3];
+
+        console.info(names);
+        console.info(value);
+    });
+})
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function(){
+    it('should must declare', function(){
+        const name: string = "Hello Eko";
+        const balance: number = 1000;
+        const isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+})
